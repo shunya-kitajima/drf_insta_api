c1a7594b005467e29a61e9b6a346842340d269e1
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-28 18:51:41 +0900

    create Post class

--- api/models.py
+++ api/models.py
@@ -8,6 +8,9 @@ def upload_avatar_path(instance, filename):
     ext = filename.split('.')[-1]
     return '/'.join(["avatars", str(instance.userProfile.id) + str(instance.nickName) + str(".") + str(ext)])
 
+def upload_post_path(instance, filename):
+
+
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None):
@@ -40,12 +43,29 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 
 class Profile(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     nickName = models.CharField(max_length=20)
     userProfile = models.OneToOneField(
         settings.AUTH_USER_MODEL, related_name="userProfile", on_delete=models.CASCADE
     )
-    created_at = models.DateTimeField(auto_now_add=True)
     img = models.ImageField(blank=True, null=True, upload_to=upload_avatar_path)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.nickName
+
+
+class Post(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    title = models.CharField(max_length=30, blank=False)
+    userPost = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name="userPost", on_delete=models.CASCADE
+    )
+    img = models.ImageField(blank=True, null=True, upload_to=upload_post_path)
+    liked = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="liked", blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
