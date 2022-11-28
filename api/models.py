f89159e9090c24c40bcf88dfba4c112e05d30db4
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-28 18:17:02 +0900

    import modules

--- api/models.py
+++ api/models.py
@@ -1,3 +1,4 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.conf import settings
 
-# Create your models here.
