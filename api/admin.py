840a8f2b7f78d348bd75e6b8985298c2732b1a0d
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-29 10:20:29 +0900

    import modules

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseAdmin
+from django.utils.translation import gettext as _
+from . import models
 
-# Register your models here.
