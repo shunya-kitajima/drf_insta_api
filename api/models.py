ce829e73abe92c10b88b1cdc79d916d68d347329
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-29 10:37:38 +0900

    modify

--- api/models.py
+++ api/models.py
@@ -23,7 +23,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_super(self, email, password):
+    def create_superuser(self, email, password):
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
