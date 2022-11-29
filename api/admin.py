9271966ab4a2557d61bfe37e00788c60b8d56e27
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-29 10:30:10 +0900

    override UserAdmin

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
 from . import models
 
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ["id"]
+    list_display = ["email", "username"]
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal Info"), {"fields": ("username", )}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser"
+                )
+            }
+        ),
+        (_("Important dates"), {"fields": ("last_login", )}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide", ),
+            "fields": ("email", "password1", "password2"),
+        }),
+    )
