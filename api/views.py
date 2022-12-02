b0e67bcd03fdd5e1d3abbba94b38ee7ae2d801c2
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-02 15:09:52 +0900

    import modules

--- api/views.py
+++ api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.permissions import AllowAny
+from . import serializers
+from .models import Profile, Post, Comment
 
 # Create your views here.
