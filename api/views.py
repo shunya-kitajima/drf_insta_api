6ad4ef98fa47f23217706517f0196dea206cdcde
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-02 15:14:04 +0900

    create CreateUserView and ProfileViewSet

--- api/views.py
+++ api/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
 from rest_framework import generics, viewsets
 from rest_framework.permissions import AllowAny
 from . import serializers
 from .models import Profile, Post, Comment
 
-# Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = serializers.UserSerializer
+    permission_classes = (AllowAny, )
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(userProfile=self.request.user)
+
