7a89125ecfa26cedbf61ac9ab087576e09aacd5f
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-02 15:20:42 +0900

    create PostViewSet

--- api/views.py
+++ api/views.py
@@ -26,7 +26,9 @@ class MyProfileListView(generics.ListAPIView):
 
 
 class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
     serializer_class = serializers.PostSerializer
 
-
+    def perform_create(self, serializer):
+        serializer.save(userPost=self.request.user)
 
