26ec95eb9bd1b69e7de0ac3c7dd9e31a419893cb
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-12-02 15:17:38 +0900

    create MyProfileListView

--- api/views.py
+++ api/views.py
@@ -16,3 +16,17 @@ class ProfileViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(userProfile=self.request.user)
 
+
+class MyProfileListView(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(userProfile=self.request.user)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.PostSerializer
+
+
+
